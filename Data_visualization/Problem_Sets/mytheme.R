b3fdc12e766c56442f9605748893227842c335e1
Kevin Middleton <middletonk@missouri.edu>
2023-01-31 15:01:09 -0600

    Update viz PS 4

---
+++ Data_visualization/Problem_Sets/mytheme.R
@@ -0,0 +1,6 @@
+library(cowplot)
+theme_set(theme_cowplot(font_size = 10))
+mytheme <- list(
+  theme(axis.title = element_text(face = "bold"),
+        axis.text = element_text(face = "italic")
+  ))
