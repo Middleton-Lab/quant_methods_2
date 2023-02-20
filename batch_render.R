0b9f6229ac83dfd6c7bc4ae6d63e13e1d9295aca
Kevin Middleton <middletonk@missouri.edu>
2023-02-20 09:30:09 -0600

    Create batch_render.R

---
+++ batch_render.R
@@ -0,0 +1,9 @@
+qmds <- list.files(pattern = "^[1-5].[1-5].*qmd$",
+                   path = "Bayesian_inference_1/",
+                   full.names = TRUE)
+
+for (qq in qmds) {
+  message("Processing ", qq)
+  syscall <- paste0("quarto render ", qq, " --to revealjs")
+  system(syscall)
+}
