76c7d8ed7659f2c79390cea5ae1e4b9b7fbd7dce
Kevin Middleton <middletonk@missouri.edu>
2023-04-03 11:27:38 -0500

    Create jackknife_sim.R

---
+++ Randomization_simulation_methods/jackknife_sim.R
@@ -0,0 +1,67 @@
+library(tidyverse)
+library(furrr)
+
+set.seed(324628764)
+
+
+biased_sd <- function(x) {
+  sqrt(sum((x - mean(x)) ^ 2) / length(x))
+}
+
+
+jackknife_sd <- function(x) {
+  ps <- numeric(length(x))
+  for (ii in 1:length(x)) {
+    x_sub <- x[-ii]
+    ps[ii] <- biased_sd(x_sub)
+  }
+  return(mean(ps))
+}
+
+sim_sds <- function(ii, xbar, xsd, n) {
+  x <- rnorm(n, xbar, xsd)
+
+  x_sd <- sd(x)
+
+  x_biased_sd <- biased_sd(x)
+
+  x_jackkife_sd <- jackknife_sd(x)
+
+  return(tibble(ii, x_sd, x_biased_sd, x_jackkife_sd))
+}
+
+n <- 100
+xbar <- 0
+xsd <- 1
+
+plan(multisession, workers = 10)
+sims <- future_map(.x = seq_len(100),
+                   .f = sim_sds,
+                   xbar = 10,
+                   xsd = 5,
+                   n = 1e4,
+                   .progress = TRUE,
+                   .options = furrr_options(seed = TRUE)) |>
+  list_rbind()
+plan(sequential)
+
+sims_long <- pivot_longer(sims, cols = -x_sd)
+
+ggplot(sims_long, aes(x = x_sd, y = value, color = name)) +
+  geom_abline(slope = 1, intercept = 0) +
+  geom_point() +
+  facet_grid(name ~ .)
+
+sims <- sims |>
+  mutate(d_biased = x_biased_sd - x_sd,
+         d_jackknife = x_jackkife_sd - x_sd)
+
+sims |>
+  select(starts_with("d")) |>
+  pivot_longer(cols = everything()) |>
+  ggplot(aes(value)) +
+  geom_vline(xintercept = 0, color = "red") +
+  geom_histogram(bins = 50) +
+  facet_grid(. ~ name) +
+  cowplot::theme_cowplot()
+
